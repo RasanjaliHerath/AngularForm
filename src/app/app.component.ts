d44e56dfbb16fcd08b2b4c05979312a456950925
RasanjaliHerath <154677088+RasanjaliHerath@users.noreply.github.com>
2024-07-02 11:56:10 +0530

    This example demonstrates how to create a basic form using Angular with HTML and CSS styling.
    This Angular project demonstrates the creation of a basic form using Angular's template-driven approach, coupled with HTML and CSS for structure and styling, respectively.
     The form includes fields for a user's full name, email address, a message textarea, and a submit button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularForm';
+  title = 'angular18forms';
+
+  onSubmit(form: NgForm) {
+    console.log('Your form data:', form.value);
+  }
 }
